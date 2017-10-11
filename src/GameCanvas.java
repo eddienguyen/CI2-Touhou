9174fbcb1446cb9402b2d59ad89da4c544ecd839
eddienguyen <eddienguyen996@gmail.com>
2017-10-11 20:45:16 +0700

    Added player, bg drawing Added size fixing, window closing handling

--- src/GameCanvas.java
+++ src/GameCanvas.java
@@ -1,9 +1,30 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class GameCanvas extends JPanel{
 
-    public GameCanvas(){
+    BufferedImage background, player_straight;
 
+    public GameCanvas(){
+        //1.Load bg:
+        try {
+            background = ImageIO.read(new File("assets/images/background/0.png"));
+            player_straight = ImageIO.read(new File("assets/images/players/straight/0.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
+    //2.draw Background:
+    @Override
+    public void paintComponent(Graphics g) {
+
+        g.drawImage(background,0,0,null);
+        g.drawImage(player_straight,175,450,null);
+
+    }
 }
--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,14 +1,30 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 public class GameWindow extends JFrame{
 
     GameCanvas canvas;
     public GameWindow()  {
         this.setSize(800,600);
-        this.setVisible(true);
 
         this.canvas = new GameCanvas();
         this.setContentPane(this.canvas);
+
+        this.canvas.setVisible(true);
+        this.setVisible(true);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        this.setResizable(false);
     }
+
+
+
+
 }
