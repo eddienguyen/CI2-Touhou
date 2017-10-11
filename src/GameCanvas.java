ec866aea12aa0bfd96f5fa614df428b713bd9c21
eddienguyen <eddienguyen996@gmail.com>
2017-10-11 19:43:12 +0700

    Added assets for vertical slice

---
+++ src/GameCanvas.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class GameCanvas extends JPanel{
+
+    public GameCanvas(){
+
+    }
+
+}
--- src/GameWindow.java
+++ src/GameWindow.java
@@ -2,9 +2,13 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GameWindow extends JFrame{
+
+    GameCanvas canvas;
     public GameWindow()  {
         this.setSize(800,600);
         this.setVisible(true);
 
+        this.canvas = new GameCanvas();
+        this.setContentPane(this.canvas);
     }
 }
